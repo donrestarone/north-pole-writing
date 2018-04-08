cd90d740bc63bbd89dff91bf25830f2f775b6b95
donrestarone <shashikejayatunge@gmail.com>
2018-04-08 12:27:59 -0400

    finish north pole writing except for 3 space bug in infraction

--- naughty_letter_writer.rb
+++ naughty_letter_writer.rb
@@ -6,11 +6,12 @@ naughty_letter = File.read('templates/naughty_sample_letter.txt.erb')
 kids_data.each_line do |kid|
 
   kid_data_array = kid.split
+  infraction_split = kid.split('|')
 
   name     = kid_data_array[0]
   behavior = kid_data_array[1]
   toys     = kid_data_array[2..7]
-  infraction = kid_data_array[13]
+  infraction = infraction_split[1]
   
   next unless behavior == 'naughty'
 
